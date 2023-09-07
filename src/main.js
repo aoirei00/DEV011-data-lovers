bd0182bac0f396fd1f7403b7f424c5b038b142ef
Karina_mr <gatica265@gmail.com>
2023-09-07 14:13:31 -0600

    Versión funcional de filtros y limpieza de filtros, usando modulos

--- src/main.js
+++ src/main.js
@@ -1,89 +1,68 @@
-import { filter } from './dataFunctions.js';
-//import { renderItems } from './view.js';
-
-// import data from './data/lol/lol.js';
+import { filterData,sortData } from './dataFunctions.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 import viewData from './view.js';
 
-const pokemones = data.pokemon
+let pokemones = data.pokemon
 
-/* console.log(filter.filterData("data", "filterBy", "value")); */
+// Botones para filtrar por typo de pokemon
+const buttonFilterWater = document.querySelector('button[value="water"]');
+const buttonFilterFire = document.querySelector('button[value="fire"]');
+const buttonFilterFight = document.querySelector('button[value="fight"]');
+const buttonFilterBug = document.querySelector('button[value="bug"]');
+const buttonFilterGrass = document.querySelector('button[value="grass"]');
+const buttonFilterGround = document.querySelector('button[value="ground"]');
+const buttonFilterFlying = document.querySelector('button[value="flying"]');
+const buttonFilterFairy = document.querySelector('button[value="fairy"]');
+const buttonFilterElectric = document.querySelector('button[value="electric"]');
+const buttonFilterNormal = document.querySelector('button[value="normal"]');
+const buttonFilterPoison = document.querySelector('button[value="poison"]');
+const buttonFilterGhost = document.querySelector('button[value="ghost"]');
+const buttonFilterDark = document.querySelector('button[value="dark"]');
+const buttonFilterIce = document.querySelector('button[value="ice"]');
+const buttonFilterPsychic = document.querySelector('button[value="psychic"]');
+const buttonFilterDragon = document.querySelector('button[value="dragon"]');
+const buttonFilterSteel = document.querySelector('button[value="steel"]');
+const buttonFilterRock = document.querySelector('button[value="rock"]');
 
-function actualizar(imagen, nombre, descripcion){
-    let output = '<li class="tarjeta bug" type="none">' +
-    '<img id="imgPokemon" class="imgPokemon" src = "' +imagen+ '"/>'+
-    '<dl itemscope itemtype="pokemon">' +
-    '<dt>Nombre:</dt><dd itemprop="name">' + nombre+ '</dd>'+
-    '<dt>Descripción:</dt><dd itemprop="about">' + descripcion + '</dd>'+
-    '</li>'
-    return output
-}
+//Botones para filtrar por rareza (legendario)
+const buttonFilterNoLegendary = document.querySelector('button[value="nolegendary"]');
+const buttonFilterLegendary = document.querySelector('button[value="legendary"]');
 
+//Botones para filtrar por generación
+const buttonFilterKanto = document.querySelector('button[value="kanto"]');
+const buttonFilterJohto = document.querySelector('button[value="johto"]');
 
-// Función para acceder a un valor de data, dado un string de llaves
-function getPropertyValue(obj, key) {
-  const keys = key.split('.');  //Divide el string en llaves separadas
-  let value = obj;  //declara el objeto que se va a ir actualizando (en diccionarios o arrays), segun vaya pasando por las llaves
-  for (const k of keys) {   //Comienza el ciclo de entrada con las llaves
-    if (value.hasOwnProperty(k)) {  //verifica que la llave exista en el objeto (en este nivel)
-        value = value[k];   //Si existe lo actualiza y entra al obeto que tenga esa llave
-    } else if (Array.isArray(value)){   //En caso de que no exista, verifica si es un array []
-        for (let i = 0; i<value.length;i++){    //si es un array, lo recorre y aplica la misma función recursiva para cada elemento del array
-            value[i]=getPropertyValue(value[i],key) //Actializa el elemento value, considerando que puede estar repetido en [i] y [j]
-        }
-    }
-  }
-  return value;
-}
+//Botones generales para cambio de vista y limpieza del filtro
+const buttonChangeView = document.querySelector('button[id="cambioVista"]');
+const buttonCleanFilter = document.querySelector('button[id="limpiarFiltro"]');
 
-//const claveFiltro = "size.height"; // Filtrar por la generación "kanto"
-//const valorFiltro = "0.99 m";
+//Lista para ordedar las tarjetas
+const selectElement = document.querySelector('select[name="ordenName"]');
 
-const claveFiltro = "quick-move.type"; // Filtrar por la generación "kanto"
-const valorFiltro = "poison";
-
-/*
-function filtroGeneracion(data,condicional){
-    let data2 = data.filter(function(entry){
-        return entry.name.includes(condicional)});
-    return data2 
-}
-
-let prueba = filtroGeneracion(pokemones,'pikachu')
-console.log(prueba)
-*/ 
-
-function filterData(data, filterBy, value){
-    const data2 = data.filter(function(pokemon){
-    const propertyValue = getPropertyValue(pokemon, filterBy); //extrae a los valores anidados (puede ser un elemento o un array)
-    return propertyValue.includes(value); //se usa includes y no === ara que traida todos los objetos con esa caracteristica (por elemento o array)
-    });
-    return data2;
-}
-//console.log("GPT",pokemonFiltrados);
-
-function namesPokeMap(data,htmlfhater){ 
-    let data2 = data.map(function(entry){
-        htmlfhater.innerHTML += actualizar(entry.img, entry.name, entry.about)
-        return htmlfhater
-    });
-return data2
-}
-
-let ulPokemon = document.getElementById("root")
-
-let pokemonFiltrados = filterData(pokemones,"size.weight","90.5 kg")
-//console.log()
-namesPokeMap (pokemonFiltrados,ulPokemon)
+const ulContentCards = document.getElementById("root");
 
 
-function sortBy(data, sortBy, sortOrder) {
-    if (sortOrder === 'asc') {
-        return data.sort((a, b) => (a[sortBy] > b[sortBy]) ? 1 : -1);
-    } else if (sortOrder === 'desc') {
-        return data.sort((a, b) => (a[sortBy] < b[sortBy]) ? 1 : -1);
-    } else {
-        return data;
-    }
-  }
+
+buttonFilterWater.addEventListener("click",function(){
+    ulContentCards.innerHTML = '';
+    pokemones = filterData(pokemones, "type", buttonFilterWater.value);
+    viewData.namesPokeMap(pokemones,ulContentCards);
+}); 
+
+buttonFilterLegendary.addEventListener("click",function(){
+    ulContentCards.innerHTML = '';
+    pokemones = filterData(pokemones, "pokemon-rarity", buttonFilterLegendary.value);
+    viewData.namesPokeMap(pokemones,ulContentCards);
+}); 
+
+buttonCleanFilter.addEventListener("click", ()=>{
+    pokemones = data.pokemon;
+    ulContentCards.innerHTML = '';
+});
+
+/* 
+selectElement.addEventListener("input",()=>{
+    ulContentCards.innerHTML = '';
+    pokemones = sortData(pokemones,selectElement.value,selectElement.sortBy)
+    viewData.namesPokeMap(pokemones,ulContentCards);
+}); */
--- src/view.js
+++ src/view.js
@@ -2,10 +2,22 @@
 
 const viewData = {
 
-  renderItems:(tipos)=>{
-    return tipos; 
-  }
-
+  renderItems:(data)=>{
+    let output = '<li class="tarjeta" type="none">' +
+    '<img id="imgPokemon" class="imgPokemon" src = "' + data.img + '"/>' +
+    '<dl itemscope itemtype="pokemon">' +
+    '<dt>Nombre:</dt><dd itemprop="name">' + data.name + '</dd>' +
+    '<dt>Descripción:</dt><dd itemprop="about">' + data.about + '</dd>' +
+    '</li>'
+    return output
+  },
+  namesPokeMap:(data,htmlfhater)=>{ 
+    let data2 = data.map(function(entry){
+      htmlfhater.innerHTML += viewData.renderItems(entry)
+      return htmlfhater
+    });
+return data2
+}
 }
 
 export default viewData;
